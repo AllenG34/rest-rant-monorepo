72f6ae4026fcb4e7cd1fa4cb0bd98fc4a0cf048b
Allen Go <goallen2001@gmail.com>
2023-08-01 01:22:58 -0700

    finished day 5

--- frontend/src/places/PlaceDetails.js
+++ frontend/src/places/PlaceDetails.js
@@ -4,6 +4,7 @@ import { CurrentUser } from "../contexts/CurrentUser";
 import CommentCard from './CommentCard'
 import NewCommentForm from "./NewCommentForm";
 
+
 function PlaceDetails() {
 
 	const { placeId } = useParams()
@@ -54,8 +55,7 @@ function PlaceDetails() {
 		const response = await fetch(`http://localhost:5000/places/${place.placeId}/comments`, {
 			method: 'POST',
 			headers: {
-				'Content-Type': 'application/json',
-				'Authorization': `Bearer ${localStorage.getItem('token')}`
+				'Content-Type': 'application/json'
 			},
 			body: JSON.stringify(commentAttributes)
 		})
@@ -105,6 +105,7 @@ function PlaceDetails() {
 		})
 	}
 
+
 	let placeActions = null
 
 	if (currentUser?.role === 'admin') {
@@ -119,7 +120,6 @@ function PlaceDetails() {
 			</>
 		)
 	}
-
 
 	return (
 		<main>
